1ca2d743e2e64ae181e8ce40d0b9943b1adf2b7c
root <root@Brian.localdomain>
2025-12-03 15:56:41 -0800

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,7 @@ import { v2 as cloudinary } from "cloudinary";
 import messageRoutes from "./routes/message.route.js";
 import {app, server} from "./lib/socket.js";
 import cors from "cors";
+import path from "path";
 dotenv.config()
 
 cloudinary.config({
@@ -21,6 +22,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve()
 
 app.use(express.json());
 app.use(cookieParser());
@@ -31,6 +33,14 @@ app.use(cors ({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 server.listen(PORT, () => {
     console.log("server running on port:" + PORT);
